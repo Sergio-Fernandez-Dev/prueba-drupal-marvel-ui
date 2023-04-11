eaf5ef0e8ad4df06fd4ab2985c98abe51a467af1
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2023-04-11 11:31:13 +0200

    create comics view

---
+++ src/views/ComicsView.vue
@@ -0,0 +1,24 @@
+u<script setup>
+import APIRequestHandler from '@/tools/APIRequestHandler';
+import { onMounted, ref } from "vue";
+
+const data = ref({});
+
+onMounted( async () => {
+  console.log(import.meta.env.VITE_MARVEL_API_URL);
+  const marvelAPI = new APIRequestHandler(import.meta.env.MARVEL_API_URL);
+  await marvelAPI.getAPIKey(import.meta.env.BACKEND_API_URL_PATH, '/marvel/apikey');
+  data.value = await marvelAPI.getData('/comics');
+}); 
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
